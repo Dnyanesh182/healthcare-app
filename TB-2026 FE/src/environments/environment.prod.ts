d4f9b32df76848c1175c6b4d2aae1bb0e2a3c3ca
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-05-09 21:42:30 +0530

    update api url

---
+++ TB-2026 FE/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://healthcare-app-39kt.onrender.com/',
+    accountName: 'hciimagestoragedev',
+    accountKey: 'I/RP3yFt9p8TUv4CCZ15myITwehXSbFHieFE6f/olAMvf/lV0FsMPP0EF/dK+8zFe/8+0RA57KD2+AStuSSfZg=='
+  };
